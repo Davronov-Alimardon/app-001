f6f40c2f3f31064f1f2cf9a22a2bd90b8aa046c4
= <emailaddr>
2024-05-12 15:20:04 +0500

    added card carousel command table datepicker

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,19 @@ import { AvatarDemo } from "@/components/avatar";
 import { Badge } from "@/components/ui/badge";
 import { BreadcrumbDemo } from "@/components/breadcrumb";
 import { CalendarDemo } from "@/components/calendar";
+import { CardWithForm } from "@/components/card";
+import { CarouselDemo } from "@/components/carousel";
+import { CheckboxDemo } from "@/components/checkbox";
+import { CollapsibleDemo } from "@/components/collapsible";
+import { CommandDemo } from "@/components/command";
+import { ContextMenuDemo } from "@/components/context-menu";
+import { DatePickerDemo } from "@/components/datepicker";
+import { DataTableDemo } from "@/components/datatable";
 
 export default function Home() {
   return (
     <div>
+      {/* Navbar */}
       <div className="m-5 flex justify-between items-center px-2 border-b pb-2">
         <div className="flex justify-normal items-center">
           <Button className="mx-2">Click me</Button>
@@ -25,19 +34,29 @@ export default function Home() {
           <AvatarDemo imgUrl="https://github.com/shadcn.png" />
         </div>
       </div>
+
+      {/* block 1 */}
       <div className="m-5 flex font-mono px-2 ">
-        <div className="border-b py-2 w-[30%]">
+        <div className=" py-2 w-[30%]">
           <BreadcrumbDemo />
         </div>
       </div>
+      <div className="p-2 m-5 border-y text-center">this is a border</div>
 
-      <div className="m-5 flex justify-between px-2">
+      {/* block 2 */}
+      <div className="m-5 flex justify-around px-2">
         <div className="max-w-[30%]">
           <h3 className="text-[18px] mb-2 text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
             Alert
           </h3>
           <div className="my-4"></div>
           <AlertDemo />
+        </div>
+        <div className="w-[30%]">
+          <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
+            Accordion
+          </h3>
+          <AccordionDemo />
         </div>
         <div className="max-w-[30%]">
           <h3 className="text-[18px] mb-2 text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
@@ -52,14 +71,27 @@ export default function Home() {
             </Badge>
           </div>
         </div>
-        <div className="w-[30%]">
-          <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
-            Accordion
-          </h3>
-          <AccordionDemo />
-        </div>
       </div>
-      <div className="m-5 flex justify-between px-2">
+      <div className="p-2 m-5 border-y text-center">this is a border</div>
+
+      {/* block 3 */}
+      <div className="m-5 flex justify-around px-2">
+        <div className="max-w-[30%]">
+          <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
+            Card Form
+          </h3>
+          <div className="m-4"></div>
+          <CardWithForm />
+        </div>
+
+        <div className="max-w-[30%]">
+          <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
+            Carousel
+          </h3>
+          <div className="m-4"></div>
+          <CarouselDemo />
+        </div>
+
         <div className="max-w-[30%]">
           <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
             Calendar
@@ -68,6 +100,62 @@ export default function Home() {
           <CalendarDemo />
         </div>
       </div>
+      <div className="p-2 m-5 border-y text-center">this is a border</div>
+
+      {/* block 4 */}
+      <div className="m-5 flex justify-around px-2">
+        {/* box 1 */}
+        <div className="max-w-[30%]">
+          <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
+            CheckBox
+          </h3>
+          <div className="m-4"></div>
+          <CheckboxDemo />
+        </div>
+
+        {/* box 2 */}
+        <div className="max-w-[30%]">
+          <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
+            Command
+          </h3>
+          <div className="m-4"></div>
+          <CommandDemo />
+        </div>
+
+        {/* box 3 */}
+        <div className="max-w-[30%]">
+          <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
+            Collapsible
+          </h3>
+          <div className="m-4"></div>
+          <CollapsibleDemo />
+        </div>
+      </div>
+      <div className="p-2 m-5 border-y text-center">this is a border</div>
+
+      {/* block 5 */}
+      <div className="m-5 flex justify-around px-2">
+        {/* box 1 */}
+        <div className="max-w-[30%]">
+          <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
+            ContextMenu
+          </h3>
+          <div className="m-4"></div>
+          <ContextMenuDemo />
+        </div>
+
+        {/* box 3 */}
+        <div className="w-[70%]">
+          <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
+            Data Table
+          </h3>
+          <div className="m-4"></div>
+          <DataTableDemo />
+        </div>
+      </div>
+      <div className="p-2 m-5 border-y text-center">this is a border</div>
+
+      {/* footer aspect ratio */}
       <footer className="m-14">
         <h3 className="text-[18px] text-uppercase hover:underline text-violet-900 font-mono font-semibold dark:text-violet-400 text-center">
           Aspect Ratio
